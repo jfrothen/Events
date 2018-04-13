8f687cd09a449deb75657772bd05e66e7d70d7c4
Joshenberg <jfrothen@uvm.edu>
2018-04-13 11:21:38 -0400

    Initial commit

---
+++ app/src/main/java/com/example/joshua/events/AllEventsActivity.java
@@ -0,0 +1,198 @@
+package com.example.joshua.events;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import android.util.Pair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+public class AllEventsActivity extends ListActivity {
+
+    // Progress Dialog
+    private ProgressDialog pDialog;
+
+    // Creating JSON Parser object
+    JSONParser jParser = new JSONParser();
+
+    ArrayList<HashMap<String, String>> eventsList;
+
+    // url to get all products list
+    private static String url_all_events = "https://localhost/EventApp/android_connect/get_all_events.php";
+
+    // JSON Node names
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_PRODUCTS = "events";
+    private static final String TAG_EID = "eid";
+    private static final String TAG_NAME = "name";
+
+    // products JSONArray
+    JSONArray events = null;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.all_events);
+
+        // Hashmap for ListView
+        eventsList = new ArrayList<HashMap<String, String>>();
+
+        // Loading products in Background Thread
+        new LoadAllEvents().execute();
+
+        // Get listview
+        ListView lv = getListView();
+
+        // on seleting single product
+        // launching Edit Product Screen
+        lv.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // getting values from selected ListItem
+                String eid = ((TextView) view.findViewById(R.id.eid)).getText()
+                        .toString();
+
+                // Starting new intent
+                Intent in = new Intent(getApplicationContext(),
+                        EditEventActivity.class);
+                // sending eid to next activity
+                in.putExtra(TAG_EID, eid);
+
+                // starting new activity and expecting some response back
+                startActivityForResult(in, 100);
+            }
+        });
+
+    }
+
+    // Response from Edit Product Activity
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // if result code 100
+        if (resultCode == 100) {
+            // if result code 100 is received
+            // means user edited/deleted product
+            // reload this screen again
+            Intent intent = getIntent();
+            finish();
+            startActivity(intent);
+        }
+
+    }
+
+    /**
+     * Background Async Task to Load all events by making HTTP Request
+     * */
+    class LoadAllEvents extends AsyncTask<String, String, String> {
+
+        /**
+         * Before starting background thread Show Progress Dialog
+         * */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(AllEventsActivity.this);
+            pDialog.setMessage("Loading events. Please wait...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(false);
+            pDialog.show();
+        }
+
+        /**
+         * getting All events from url
+         * */
+        protected String doInBackground(String... args) {
+            // Building Parameters
+
+            HashMap<String, String> hmap = new HashMap<String, String>(0);
+            // getting JSON string from URL
+            JSONObject json = jParser.makeHttpRequest(url_all_events, "GET", hmap);
+
+            // Check your log cat for JSON reponse
+            Log.d("All Products: ", json.toString());
+
+            try {
+                // Checking for SUCCESS TAG
+                int success = json.getInt(TAG_SUCCESS);
+
+                if (success == 1) {
+                    // products found
+                    // Getting Array of Products
+                    events = json.getJSONArray(TAG_PRODUCTS);
+
+                    // looping through All Events
+                    for (int i = 0; i < events.length(); i++) {
+                        JSONObject c = events.getJSONObject(i);
+
+                        // Storing each json item in variable
+                        String id = c.getString(TAG_EID);
+                        String name = c.getString(TAG_NAME);
+
+                        // creating new HashMap
+                        HashMap<String, String> map = new HashMap<String, String>();
+
+                        // adding each child node to HashMap key => value
+                        map.put(TAG_EID, id);
+                        map.put(TAG_NAME, name);
+
+                        // adding HashList to ArrayList
+                        eventsList.add(map);
+                    }
+                } else {
+                    // no products found
+                    // Launch Add New product Activity
+                    Intent i = new Intent(getApplicationContext(),
+                            NewEventActivity.class);
+                    // Closing all previous activities
+                    i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(i);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        /**
+         * After completing background task Dismiss the progress dialog
+         * **/
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog after getting all products
+            pDialog.dismiss();
+            // updating UI from Background Thread
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    /**
+                     * Updating parsed JSON data into ListView
+                     * */
+                    ListAdapter adapter = new SimpleAdapter(
+                            AllEventsActivity.this, eventsList,
+                            R.layout.list_event, new String[] { TAG_NAME},
+                            new int[] { R.id.eid, R.id.name });
+                    // updating listview
+                    setListAdapter(adapter);
+                }
+            });
+        }
+    }
+}
---
+++ app/src/main/java/com/example/joshua/events/MainScreenActivity.java
@@ -0,0 +1,54 @@
+package com.example.joshua.events;
+
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+public class MainScreenActivity extends AppCompatActivity {
+
+    Button btnViewProducts;
+    Button btnNewProduct;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_screen);
+
+        // Buttons
+        btnViewProducts = (Button) findViewById(R.id.btnViewEvents);
+        btnNewProduct = (Button) findViewById(R.id.btnCreateEvent);
+
+        // view products click event
+        btnViewProducts.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                // Launching All products Activity
+                Intent i = new Intent(getApplicationContext(), AllEventsActivity.class);
+                startActivity(i);
+
+            }
+        });
+
+        // view products click event
+        btnNewProduct.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                // Launching create new event activity
+                Intent i = new Intent(getApplicationContext(), NewEventActivity.class);
+                startActivity(i);
+
+            }
+        });
+    }
+}
